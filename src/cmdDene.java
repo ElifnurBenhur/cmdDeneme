e4d32b96b453a23cbd1939a4fe8843b0ff234411
ElifnurBenhur <80223082+ElifnurBenhur@users.noreply.github.com>
2021-08-20 08:46:22 +0300

    Update cmdDene.java

--- src/cmdDene.java
+++ src/cmdDene.java
@@ -1,5 +1,6 @@
 public class cmdDene {
     public static void main(String[] args){
         System.out.println("Bilgisayar versiyon 1");
+        System.out.println("Respiratory den yapılan değişim");
     }
 }
